b629761dd059b4928b566c74ff323b8ce54cf33c
Lucas Fontes <lucas.fontes@brlink.com.br>
2022-06-08 12:10:59 -0300

    Change Backend and Provider

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    key     = "s3/s3-web.tfsate"
+    key     = "s3/s3-web-jenkins.tfsate"
     bucket  = "backend-lfg"
     region  = "us-west-2"
-    profile = "pocaccess"
   }
 }
