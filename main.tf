803652f4f7347d7950bea200e18e25ea85b012ff
Lucas Fontes <lucas.fontes@brlink.com.br>
2022-06-08 16:36:20 -0300

    source errada

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-
+/*
 module "s3web" {
     source = "./modules/s3"
     bucket_name = "lfg"
     produto = "veiculos"
 }
+*/
 
-/*
+
 module "s3webbad" {
     source = "./modules/bads3"
     bucket_name = "lfg"
     produto = "Veiculos"
 }
-*/
+
 
