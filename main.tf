a3d8761a8a01e38681f8bacb4b1afe3672d9b83a
Lucas Fontes <lucas.fontes@brlink.com.br>
2022-06-08 16:37:44 -0300

    tag correta

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
-/*
+
 module "s3web" {
     source = "./modules/s3"
     bucket_name = "lfg"
-    produto = "veiculos"
+    produto = "Veiculos"
 }
-*/
 
 
+/*
 module "s3webbad" {
     source = "./modules/bads3"
     bucket_name = "lfg"
     produto = "Veiculos"
 }
+*/
 
 
